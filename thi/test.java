e61781a2851104990e0de630de5bcd87a7367981
truongson00 <soi30082000@gmail.com>
2021-11-28 14:56:34 +0700

    commit 2

--- thi/test.java
+++ thi/test.java
@@ -3,5 +3,6 @@ package thi;
 public class test {
     public static void main(String[] args) {
         System.out.println("commit to github");
+        System.out.println("commit 2 to git hub");
     }
 }
