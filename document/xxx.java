42ff228fab3af0686e93f5430b8688c0258d0244
truongson00 <soi30082000@gmail.com>
2021-11-30 19:00:25 +0700

    xxx

---
+++ document/xxx.java
@@ -0,0 +1,18 @@
+package document;
+
+public class xxx {
+    /**
+     * 
+     * @param a integer a
+     * @param b integer b
+     * @return sum integer a addition integer b
+     */
+    public static int sum(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        sum(1, 2);
+    }
+}
